0c3159e63b158d0918e5f91c4db85bbe227bd42e
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2024-06-30 22:17:38 +0200

    add OpenTK

---
+++ src/VoxelTK.Client/Game.cs
@@ -0,0 +1,45 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace VoxelTK.Client;
+
+public class Game() : GameWindow(GameWindowSettings.Default, new NativeWindowSettings
+{
+    Title = "VoxelTK",
+    ClientSize = (800, 600)
+})
+{
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    }
+
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+    {
+        base.OnFramebufferResize(e);
+        
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+        
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+        
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        SwapBuffers();
+    }
+}
--- src/VoxelTK.Client/Program.cs
+++ src/VoxelTK.Client/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VoxelTK.Client;
+
+try
+{
+    Console.WriteLine("Initializing game...");
+    using var game = new Game();
+    Console.WriteLine("Game is now running.");
+    game.Run();
+    Console.WriteLine("Game has exited.");
+}
+catch (Exception exception)
+{
+    Console.WriteLine("Game has run into an error!");
+    Console.Error.WriteLine(exception);
+}
